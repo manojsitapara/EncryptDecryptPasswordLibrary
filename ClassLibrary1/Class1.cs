b4f13ac62ce1d5647ad03ca96e4d4e6bab03bf9f
Manoj Sitapara <manojsitapara@gmail.com>
2018-03-07 17:26:30 +0530

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+
+        private  string _encodedKey= "V1N0ZmwwUW5HMWNUYitTb3VFcXlnQT09LHA3ekpzaFc2M1VJdUdzT2NYcWlvYUFOOXpwNVlKWFB1WFFLcGNNaUJNVVk9";
+        private int _keySize = 256;
+        #region Encryption
+        
+
+        /// <summary>
+        /// Encrypt
+        /// </summary>
+        public string Encrypt(string iPlainStr)
+        {
+            RijndaelManaged aesEncryption = new RijndaelManaged();
+            aesEncryption.KeySize = _keySize;
+            aesEncryption.BlockSize = 128;
+            aesEncryption.Mode = CipherMode.CBC;
+            aesEncryption.Padding = PaddingMode.PKCS7;
+            aesEncryption.IV = Convert.FromBase64String(ASCIIEncoding.UTF8.GetString(Convert.FromBase64String(_encodedKey)).Split(',')[0]);
+            aesEncryption.Key = Convert.FromBase64String(ASCIIEncoding.UTF8.GetString(Convert.FromBase64String(_encodedKey)).Split(',')[1]);
+            byte[] plainText = ASCIIEncoding.UTF8.GetBytes(iPlainStr);
+            ICryptoTransform crypto = aesEncryption.CreateEncryptor();
+            byte[] cipherText = crypto.TransformFinalBlock(plainText, 0, plainText.Length);
+            return Convert.ToBase64String(cipherText);
+        }
+
+        /// <summary>
+        /// Decrypt
+        /// </summary>
+        public string Decrypt(string iEncryptedText)
+        {
+            RijndaelManaged aesEncryption = new RijndaelManaged();
+            aesEncryption.KeySize = _keySize;
+            aesEncryption.BlockSize = 128;
+            aesEncryption.Mode = CipherMode.CBC;
+            aesEncryption.Padding = PaddingMode.PKCS7;
+            aesEncryption.IV = Convert.FromBase64String(ASCIIEncoding.UTF8.GetString(Convert.FromBase64String(_encodedKey)).Split(',')[0]);
+            aesEncryption.Key = Convert.FromBase64String(ASCIIEncoding.UTF8.GetString(Convert.FromBase64String(_encodedKey)).Split(',')[1]);
+            ICryptoTransform decrypto = aesEncryption.CreateDecryptor();
+            byte[] encryptedBytes = Convert.FromBase64CharArray(iEncryptedText.ToCharArray(), 0, iEncryptedText.Length);
+            return ASCIIEncoding.UTF8.GetString(decrypto.TransformFinalBlock(encryptedBytes, 0, encryptedBytes.Length));
+        }
+        #endregion
+    }
+
+}
